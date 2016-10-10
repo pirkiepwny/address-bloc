59e0879c218a6788bcca8bce4c0950708a14a63b
Oliver Pirkfellner <oliver.pirkfellner.dev@gmail.com>
2016-10-10 11:45:03 +0200

    assignment-5-models-complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -15,8 +15,24 @@ RSpec.describe AddressBook do
        book = AddressBook.new
        expect(book.entries.size).to eq(0)
      end
+   end
 
-  end
+   context "#remove_entry" do
+     it "removes an entry using the name, phone_number, and email address" do
+       book = AddressBook.new
+       book.add_entry("Michael Faraday", "010.210.5182", "michael.faraday@faraday.com")
+
+       name = "Ada Lovelace"
+       phone_number = "010.012.1815"
+       email_address = "augusta.king@lovelace.com"
+       book.add_entry(name, phone_number, email_address)
+
+       expect(book.entries.size).to eq 2
+       book.remove_entry(name, phone_number, email_address)
+       expect(book.entries.size).to eq 1
+       expect(book.entries.first.name).to eq("Michael Faraday")
+   end
+ end
 
   describe "#add_entry" do
     it "adds only one entry to the address book" do
